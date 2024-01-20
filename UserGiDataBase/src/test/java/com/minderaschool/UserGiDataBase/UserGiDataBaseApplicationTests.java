8890d5a9a7206c5b2266df12a5982f714b9fb137
PedroBarge <pedro525tavares@gmail.com>
2024-01-20 10:42:23 +0000

    Users with data base

---
+++ UserGiDataBase/src/test/java/com/minderaschool/UserGiDataBase/UserGiDataBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minderaschool.UserGiDataBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserGiDataBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
